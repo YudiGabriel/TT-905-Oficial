48016cdfd00622ae4cb49525ba4f0d6587d17cd6
YudiGabriel <g235102@dac.unicamp.br>
2021-05-27 15:44:28 -0300

    Teste27

--- index.js
+++ index.js
@@ -1,71 +1,63 @@
- const express = require("express");
- const app = express();
+const express = require("express");
+const app = express();
 app.use(express.json());
-
 //Permissões
- var cors = require('cors')
- app.use(cors());
+var cors = require('cors')
+app.use(cors());
 
- //Porta que estou ouvindo
+//Porta que estou ouvindo
 
- app.listen(process.env.PORT || 3000);
+app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
- app.get('/', function (req, res){
-        res.send("Olá esta é minha atividade 7");
-     }
- ); 
-
- app.get('/hello', function (req, res){
-        res.send("Olá professor!");
-     }
- );
-
- myBooks = [ 
-    {title: "titulo1", author: "C", originalYearOfPublication: 1952},
-    {title: "titulo2", author: "N", originalYearOfPublication: 2004},
-    {title: "titulo3", author: "Al", originalYearOfPublication: 1999},
-    {title: "titulo4", author:"J", originalYearOfPublication: 2017}
-];
-
- app.get('/mensagens', function(req, res){
-     res.send(dado.filter(Boolean));
+app.get('/', function (req, res){
+       res.send("Hello WorlAafkjsdyd");
+    }
+); 
+app.get('/hello', function (req, res){
+       res.send("Olá professor!");
     }
 );
-
+const mensagens = [
+   "Gabriel Yudi", "Marcelo Miranda"
+];
+app.get('/mensagens', function(req, res){
+   // res.send(mensagens);
+    res.send(mensagens.filter(Boolean));
+   }
+);
 app.get('/mensagens/:id', function(req, res)
-    {
-        const id = req.params.id - 1;
-        const mensagem = mensagens[id];
-        
-        if(!mensagem){
-            res.send('Mensagem não encontrada');
-        } else {
-            res.send(mensagem);
-        }
-    }
+   {
+       const id = req.params.id - 1;
+       const mensagem = mensagens[id];
+       
+       if(!mensagem){
+           res.send('Mensagem não encontrada');
+       } else {
+           res.send(mensagem);
+       }
+   }
 );
 app.post('/mensagens',
-        function(req, res){
-            console.log(req.body.mensagem);
-            const mensagem = req.body.mensagem ;
-            dado.push(mensagem);
-            res.send("criar uma mensagem");
-        }
-    );
-
-    app.put('/mensagens/:id',
-     (req, res) => {
-        const id = req.params.id - 1;
-        const mensagem = req.body.mensagem;
-        mensagens[id] = mensagem;
-        res.send("Mensagem atualizada com sucesso")  
-    }
+       function(req, res){
+           console.log(req.body.mensagem);
+           const mensagem = req.body.mensagem ;
+           mensagens.push(mensagem);
+           res.send("criar uma mensagem");
+       }
+   );
+   app.put('/mensagens/:id',
+    (req, res) => {
+       const id = req.params.id - 1;
+       const mensagem = req.body.mensagem;
+       mensagens[id] = mensagem;
+       res.send("Mensagem atualizada com sucesso")  
+   }
 );
-
 app.delete('/mensagens/:id',
-        (req, res) => {
-           const id = req.params.id -1;
-           delete mensagens[id];
-           res.send("Mensagem removida com sucesso")
-        }
-    );
+       (req, res) => {
+          const id = req.params.id -1;
+          delete mensagens[id];
+          res.send("Mensagem removida com sucesso")
+       }
+   );
