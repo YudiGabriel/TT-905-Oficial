814d21514e82abb960e1ecc2248c9f6f6a10900d
YudiGabriel <g235102@dac.unicamp.br>
2021-05-27 16:30:51 -0300

    ujgvh;ok

--- index.js
+++ index.js
@@ -14,37 +14,57 @@ app.use(express.json());
         res.send("Olá esta é minha atividade 7");
      }
  ); 
+//  let LoL = {
+//      nome: "League of Legends",
+//      time: "5 pessoas",
+//      estilo:"MOBA",
+//      joga: ["Gabriel","Marcelo"]
+//  };
+//  let CoD = {
+//     nome: "Call of Duty",
+//     time: "5 pessoas",
+//     estilo:"FPS",
+//     joga: ["Gabriel","Marcelo"]
+// };
+// let Genshin = {
+//     nome: "Genshin Impact",
+//     time: "1 pessoa",
+//     estilo:"RPG",
+//     joga: "Marcelo"
+// };
 
- app.get('/hello', function (req, res){
-        res.send("Olá professor!");
-     }
- );
-
- const mensagens = ["Gabriel Yudi", "Marcelo Miranda"];
-
- app.get('/mensagens', function(req, res){
-    // res.send(mensagens);
-     res.send(mensagens.filter(Boolean));
+// let jogos = [LoL, CoD, Genshin];
+jogos = [
+    {
+        title: "LoL", who:"Gabriel"
+    },
+    {
+        title:"Ablion", who:"Marcelo"
+    }
+];
+app.get('/jogos', function(req, res){
+     res.send(jogos);
+    // res.send(jogos.filter(Boolean));
     }
 );
 
-app.get('/mensagens/:id', function(req, res)
+app.get('/jogos/:id', function(req, res)
     {
         const id = req.params.id - 1;
-        const mensagem = mensagens[id];
+        const jogo = jogos[id];
         
-        if(!mensagem){
+        if(!jogo){
             res.send('Mensagem não encontrada');
         } else {
-            res.send(mensagem);
+            res.send(jogo);
         }
     }
 );
-app.post('/mensagens',
+app.post('/jogos',
         function(req, res){
-            console.log(req.body.mensagem);
-            const mensagem = req.body.mensagem ;
-            mensagens.push(mensagem);
+            console.log(req.body.jogo);
+            const jogo = req.body.jogo ;
+            jogos.push(jogo);
             res.send("criar uma mensagem");
         }
     );
